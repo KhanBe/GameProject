c428a62695e00bb7db04d3c01241ae085520ff71
WooJaeHak <wistm6980@gmail.com>
2021-04-25 16:57:47 +0900

    Knife
    add Knife, spawnKnife

---
+++ Assets/Scripts/KnifeMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    BoxCollider2D boxCollider;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        rigid.AddForce(Vector2.up * 15, ForceMode2D.Impulse);
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    void OnBecameInvisible()//객체가 화면밖에 안보일때 호출되는 함수
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/KnifeSpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeSpawnManager : MonoBehaviour
+{
+    public bool enableSpawn = false;
+    public GameObject Knife;
+    Transform Transform;
+
+    void Awake()
+    {
+        Transform = GetComponent<Transform>();
+    }
+    void Start()
+    {
+        InvokeRepeating("SpawnKnife", 1, 2);
+    }
+
+
+    void SpawnKnife()
+    {
+
+        if (enableSpawn)
+        {
+            Instantiate(Knife, new Vector3(Transform.position.x, Transform.position.y, 0), Quaternion.identity);//복제함수
+        }
+    }
+}
