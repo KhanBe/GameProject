21d09de2575fcc49d8f4445aed7f8d68afb02af4
WooJaeHak <wistm6980@gmail.com>
2021-04-29 23:45:11 +0900

    stage 1 clear, knifeSpawn
    칼 스폰 조건 : 근처로 오면 하나 뿜기

--- Assets/Scripts/KnifeSpawnManager.cs
+++ Assets/Scripts/KnifeSpawnManager.cs
@@ -7,6 +7,7 @@ public class KnifeSpawnManager : MonoBehaviour
     public bool enableSpawn = false;
     public GameObject Knife;
     Transform Transform;
+    public PlayerMove player;
 
     void Awake()
     {
@@ -14,16 +15,30 @@ public class KnifeSpawnManager : MonoBehaviour
     }
     void Start()
     {
-        InvokeRepeating("SpawnKnife", 1, 2);
+        //InvokeRepeating("SpawnKnife", 1, 2);
+    }
+    void FixedUpdate()
+    {
+        float PlayerPositionX = player.positionX;
+        if( Transform.position.x-0.5f <= player.positionX && player.positionX<= Transform.position.x + 0.5f&& enableSpawn == true)
+        {
+            Instantiate(Knife, new Vector3(Transform.position.x, Transform.position.y, 0), Quaternion.identity);
+            enableSpawn = false;
+            Invoke("OnEnable", 1);
+        }
+            
     }
 
-
-    void SpawnKnife()
+    void OnEnable()
     {
-
+        enableSpawn = true;
+    }
+    /*void SpawnKnife()
+    {
+        
         if (enableSpawn)
         {
             Instantiate(Knife, new Vector3(Transform.position.x, Transform.position.y, 0), Quaternion.identity);//복제함수
         }
-    }
+    }*/
 }
