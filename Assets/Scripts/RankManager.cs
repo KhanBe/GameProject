fb865e7b63ff938baf3e07cb200c41362c9cb2ae
WooJaeHak <wistm6980@gmail.com>
2024-11-13 02:09:24 +0900

    link firebase realtimedatabase
    jsonfile이 유니티용이 아니어서 여태 연동이 되지 않았음

--- Assets/Scripts/RankManager.cs
+++ Assets/Scripts/RankManager.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 using System;
 using Firebase;
 using Firebase.Database;
 using Firebase.Extensions;
+using Firebase.Auth;
 
 public class Rank
-{
+{   
     public string id;
     public int score;
     public int time;
@@ -27,13 +29,18 @@ public class Rank
 public class RankManager : MonoBehaviour
 {
     public static RankManager instanceData;
-    //private Firebase.FirebaseApp app;
-
+    
     public static UIData UIinstance;
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+
+    public TMP_InputField email;
+    public TMP_InputField password;
 
     //int Death = UIinstance.DiedCount;  
 
-    private void Awake()
+    /*private void Awake()
     {
         if (instanceData != null)
         {
@@ -44,10 +51,11 @@ public class RankManager : MonoBehaviour
             instanceData = this;
             DontDestroyOnLoad(gameObject);
         }
+    }*/
 
-        // URL을 명시적으로 설정
-        FirebaseApp app = FirebaseApp.DefaultInstance;
-        app.Options.DatabaseUrl = new System.Uri("https://woo-game-db-default-rtdb.firebaseio.com/");
+    private void Start() {
+        auth = FirebaseAuth.DefaultInstance;
+
     }
 
     public void SaveRank(int rank, string userId, int score, int time, int death)
@@ -69,5 +77,51 @@ public class RankManager : MonoBehaviour
 
     public void OnBtnSubmit() {
         SaveRank(6, "testtest", 113355, 123, 4444);
+    }
+
+    
+    public void Create() {
+        auth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("회원가입 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                //이메일 비정상,간단한 비밀번호, 이메일 중복
+                Debug.LogError("회원가입 실패");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result.User;
+            Debug.LogError("회원가입 완료");
+         });
+    }
+
+    public void LogIn()
+    {
+        auth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("로그인 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                //이메일 비정상,간단한 비밀번호, 이메일 중복
+                Debug.LogError("로그인 실패");
+                return;
+            }
+
+            FirebaseUser newUser = task.Result.User;
+            Debug.LogError("로그인 완료");
+        });    
+    }
+    public void LogOut()
+    {
+        auth.SignOut();
+        Debug.LogError("로그아웃 완료");
     }
 }
