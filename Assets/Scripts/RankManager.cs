878cdfb2263ff90c23450c145b7d7f4eb74c2cc8
WooJaeHak <wistm6980@gmail.com>
2024-12-10 23:07:33 +0900

    db에 Username 유무 확인

--- Assets/Scripts/RankManager.cs
+++ Assets/Scripts/RankManager.cs
@@ -13,10 +13,10 @@ using Firebase.Auth;
 public class Rank
 {   
     public string id;
-    public int time;
+    public string time;
     public int death;
 
-    public Rank(string id, int time, int death)
+    public Rank(string id, string time, int death)
     {
         this.id = id;
         this.time = time;
@@ -45,6 +45,7 @@ public class RankManager : MonoBehaviour
     public TMP_Text profileUserNameText;
     public TMP_Text profileUserTimeText;
     public TMP_Text profileUserDeathText;
+    public TMP_Text errorUsernameText;
 
     public TMP_InputField usernameInput;
 
@@ -52,7 +53,12 @@ public class RankManager : MonoBehaviour
     public string userName = "";
     
     private DatabaseReference db;
+    public string dburl = "https://woo-game-db-default-rtdb.firebaseio.com/";
 
+
+    public void SignInWithUsername() {
+        StartCoroutine(CheckUserExistInDatabase());
+    }
 
     private void Start() {
         
@@ -60,12 +66,13 @@ public class RankManager : MonoBehaviour
     }
 
     void firebaseInit() {
-        db = FirebaseDatabase.DefaultInstance.GetReference("/Leaderbaord/");
+        db = FirebaseDatabase.DefaultInstance.GetReference("/Leaderboard/");
 
+        //db Leaderboard 데이터 자식값이 바뀌는 델리게이트 매핑(함수참조를 통해)
         db.ChildAdded += HandleChildAdded;
     }
 
-    //Firebase 데이터베이스의 특정 노드에 새로운 자식 노드가 추가될 때 호출
+    //Firebase 데이터베이스의 특정 노드(Leaderboard)에 새로운 자식 노드가 추가될 때 호출
     void HandleChildAdded(object sender, ChildChangedEventArgs args) {
         if (args.DatabaseError != null) {
             return;
@@ -78,34 +85,66 @@ public class RankManager : MonoBehaviour
 
     void GetTotalUser() {
 
-        db.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+        db.ValueChanged += (object sender2, ValueChangedEventArgs e2) => {  
             if (e2.DatabaseError != null) {
                 Debug.LogError(e2.DatabaseError.Message);
                 return;
             }
 
             totalUser = int.Parse(e2.Snapshot.ChildrenCount.ToString());
+            
+            Debug.LogError("TotalUser : " + totalUser);
         };
     }
 
+    //Username이 존재하는지 확인 하는 함수
     IEnumerator CheckUserExistInDatabase() {
+        var task =  db.OrderByChild("Username").EqualTo(usernameInput.text).GetValueAsync();
+        yield return new WaitUntil(() => task.IsCompleted);
 
+        if (task.IsFaulted) {
+            Debug.LogError("Invalid Error");
+            errorUsernameText.text = "Invalid Error";
+        }
+        else if (task.IsCompleted) {
+            DataSnapshot snapshot = task.Result;
+
+            if (snapshot != null && snapshot.HasChildren) {
+                Debug.LogError("Username Exist");
+
+                errorUsernameText.text = "Username Already Exist";
+            }
+            else {
+                Debug.LogError("Username Not Exist");
+
+                //새로운 data 넣기
+                PushUserData();
+                PlayerPrefs.SetInt("PlayerID", totalUser);
+                PlayerPrefs.SetString("Username", usernameInput.text);
+
+            }
+        }
     }
 
     IEnumerator FetchUserProfileData(string playerID)
     {
-
+        yield return null;
     }
+
     IEnumerator FetchLeaderboardData()
     {
-
+        yield return null;
     }
+
     void DisplayLeaderboardData()
     {
 
     }
 
-    public string dburl = "https://woo-game-db-default-rtdb.firebaseio.com/";
-
+    void PushUserData() {
+        db.Child("User_" + (totalUser + 1).ToString()).Child("Username").SetValueAsync(usernameInput.text);
+        db.Child("User_" + (totalUser + 1).ToString()).Child("Time").SetValueAsync("00:00:00");
+        db.Child("User_" + (totalUser + 1).ToString()).Child("Death").SetValueAsync(totalUser + 1);
+    }
 
 }
